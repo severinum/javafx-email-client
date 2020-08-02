cb0cbc500a5b9b35407024d01ead53c7c033f2f5
SeverinuM <sewerynmichota@gmail.com>
2020-08-02 18:18:36 +0100

    first commit

---
+++ src/com/severinu/Launcher.java
@@ -0,0 +1,25 @@
+package com.severinu;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent parent = FXMLLoader.load(getClass().getResource("view/LoginWindow.fxml"));
+
+        Scene scene = new Scene(parent);
+        stage.setScene(scene);
+        stage.setTitle("JavaFX Email Client");
+        stage.show();
+    }
+}
---
+++ src/com/severinu/view/LoginWindowController.java
@@ -0,0 +1,29 @@
+package com.severinu.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginWindowController {
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private TextField txtEmailAddress;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private Label lblError;
+
+    @FXML
+    void loginButtonAction() {
+        System.out.println("login button was clicked as mad !");
+    }
+
+}
